7169da1e42a1f2caf6536186f73526feaed1ea6c
MAbdurahman <mdbdrrhm@gmail.com>
2024-09-14 14:05:23 -0500

    Add joi package and add to backend/server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
+import Joi from 'joi';
 
 import Template from "./template.js";
 import NotFound from './notFound.js';
@@ -14,10 +15,10 @@ colors.enable();
 //**************** data resources ****************//
 const courses = [
    {id:100, name: 'Master JavaScript'},
-   {id:200, name: 'Master NodeJS'},
-   {id:300, name: 'Master ExpressJS'},
-   {id:400, name: 'Master JavaScript Algorithms'},
-   {id:500, name: 'Master JavaScript Data Structures'},
+   {id:200, name: 'Master JavaScript Algorithms'},
+   {id:300, name: 'Master JavaScript Data Structures'},
+   {id:400, name: 'Master NodeJS'},
+   {id:500, name: 'Master ExpressJS'},
 ]
 
 //**************** variables ****************//
@@ -46,6 +47,7 @@ app.get('/api/v1.0/courses/', (req, res) => {
 
 app.get('/api/v1.0/courses/:id', (req, res) => {
    let found = courses.find((course) => course.id === parseInt(req.params.id));
+
    if (!found) {
       res.status(404).send(NotFound());
 
@@ -53,3 +55,82 @@ app.get('/api/v1.0/courses/:id', (req, res) => {
       res.status(200).json(found);
    }
 });
+
+app.post('/api/v1.0/courses', (req, res) => {
+   const {error} = validateCourse(req.body);
+
+   if (error) {
+      return res.status(400).send(error.details[0].message);
+   }
+
+   let tempCourses = courses.map((course) => course.id);
+   let newId = tempCourses.length > 0 ? Math.max.apply(Math, tempCourses) + 100 : 100;
+
+   let newItem = {
+      id: newId,
+      name: req.body.name,
+   }
+
+   courses.push(newItem);
+
+   res.status(201).send(newItem).json({
+      'message': "Course successfully created!",
+   });
+});
+
+app.put('/api/v1.0/courses/:id', (req, res) => {
+   let found = courses.find((course) => course.id === parseInt(req.params.id));
+   if (!found) {
+      res.status(404).send(NotFound());
+
+   }
+   const {error} = validateCourse(req.body);
+
+   if (error) {
+      return res.status(400).send(error.details[0].message);
+
+   }
+   if (found) {
+      let updatedCourse = {
+         id: found.id,
+         name: req.body.name,
+      };
+
+      let targetIndex = courses.indexOf(found);
+
+      courses.splice(targetIndex, 1, updatedCourse);
+
+      res.status(201).send(updatedCourse);
+
+   } else {
+      res.status(404).json({
+         'message': 'Unable to course, because data to update does not match fields!'
+      });
+   }
+});
+
+app.delete('/api/v1.0/courses/:id', (req, res) => {
+   let found = courses.find((course) => course.id === parseInt(req.params.id));
+
+   if (!found) {
+      res.status(404).send(NotFound());
+
+   } else {
+      let targetIndex = courses.indexOf(found);
+
+      courses.splice(targetIndex, 1);
+
+      res.send(found).status(204);
+   }
+});
+
+
+//**************** helper functions ****************//
+function validateCourse(course) {
+   const schema = Joi.object().keys({
+      name: Joi.string().min(3).required()
+   });
+
+   return schema.validate(course);
+
+}//end of validateCourse Function
